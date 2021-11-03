3fd2717ae9604e6336388dbd6f27f36bd20f4e17
maksyliator <maksim.tankov@gmail.com>
2021-11-03 20:53:29 +0300

    Update files

--- src/difference-calculator.php
+++ src/difference-calculator.php
@@ -4,28 +4,40 @@ namespace difference\calculator;
 
 use function stylish\stylish;
 
-function genDiff(object $data1, object $data2): string
+function genDiff($data1, $data2)
 {
     $result = [];
-    if (is_object($data1) & is_object(($data2))) {
-        $data1 = (array) $data1;
-        $data2 = (array) $data2;
-        $keys = array_merge(array_keys($data1), array_keys($data2));
-        sort($keys);
-        $keys = array_unique($keys);
-        foreach ($keys as $key) {
-            if (!array_key_exists($key, $data1)) {
-                $result['+ ' . $key] = $data2[$key];
-            } elseif (!array_key_exists($key, $data2)) {
-                $result['- ' . $key] = $data1[$key];
-            } elseif ($data1[$key] !== $data2[$key]) {
-                $result['- ' . $key] = $data1[$key];
-                $result['+ ' . $key] = $data2[$key];
+    $data1 = (array) $data1;
+    $data2 = (array) $data2;
+    $keys = array_merge(array_keys($data1), array_keys($data2));
+    sort($keys);
+    $keys = array_unique($keys);
+    foreach ($keys as $key) {
+        if (array_key_exists($key, $data1)) {
+            if (array_key_exists($key, $data2)) {
+                if (!is_object($data1[$key]) & !is_object($data2[$key])) {
+                    if ($data1[$key] !== $data2[$key]) {
+                        $result['- ' . $key] = $data1[$key];
+                        $result['+ ' . $key] = $data2[$key];
+                    } else {
+                        $result['  ' . $key] = $data2[$key];
+                    }
+                } else {
+                    $data1 = (array) $data1[$key];
+                    $data2 = (array) $data2[$key];
+                    $result[$key] = array_map(
+                        fn($data1, $data2) => genDiff($data1, $data2),
+                        $data1,
+                        $data2
+                    );
+                    return $result;
+                }
             } else {
-                $result['  ' . $key] = $data2[$key];
+                $result['- ' . $key] = $data1[$key];
             }
+        } else {
+            $result['+ ' . $key] = $data2[$key];
         }
     }
-    var_dump($result);
     return stylish($result);
 }
