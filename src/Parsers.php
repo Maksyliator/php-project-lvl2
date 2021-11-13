32ac19f743c02c99f6b195c6e897691b941c4d5f
maksyliator <maksim.tankov@gmail.com>
2021-11-13 12:33:24 +0300

    Fix parser.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,7 +9,11 @@ function convertingFile(string $fileContent, string $extension): object
     switch ($extension) {
         case 'json':
             return json_decode($fileContent);
-        case 'yaml' || 'yml':
+        case 'yaml':
             return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
+        case 'yml':
+            return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
+        default:
+            throw new \Exception("Invalid extension. Extension must be in JSON, YAML or YML");
     }
 }
