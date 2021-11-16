cab57ceda7972a26e109f397d815eab77f50f40c
maksyliator <maksim.tankov@gmail.com>
2021-11-16 11:05:28 +0300

    Fix DifferenceTest

--- tests/DifferenceTest.php
+++ tests/DifferenceTest.php
@@ -8,15 +8,23 @@ use function Differ\Differ\genDiff;
 
 class DifferenceTest extends TestCase
 {
+    public const PATHTOFIRSTJSONFILES = 'tests/fixtures/file1_tree_structure.json';
+    public const PATHTOSECONDJSONFILES = 'tests/fixtures/file2_tree_structure.json';
+    public const PATHTOFIRSTYAMLFILES = 'tests/fixtures/file1_tree_structure.yaml';
+    public const PATHTOSECONDYAMLFILES = 'tests/fixtures/file2_tree_structure.yaml';
+
     /**
-     * @dataProvider fixturesProvider
+     * @dataProvider argumentProviderForFlatStructure
+     * @dataProvider argumentProviderForTreeStructureStylishFormat
+     * @dataProvider argumentProviderForFlatStructure
+     * @dataProvider argumentProviderForTreeStructureJsonFormat
      */
     public function testGenDiff($firstPath, $secondPath, $expected, $style = 'stylish')
     {
         $this->assertEquals($expected, genDiff($firstPath, $secondPath, $style));
     }
 
-    public function fixturesProvider(): array
+    public function argumentProviderForFlatStructure(): array
     {
         $firstPathFlatJson = 'tests/fixtures/file1_flat_structure.json';
         $secondPathFlatJson = 'tests/fixtures/file2_flat_structure.json';
@@ -24,32 +32,47 @@ class DifferenceTest extends TestCase
         $firstPathFlatYaml = 'tests/fixtures/file1_flat_structure.yaml';
         $secondPathFlatYml = 'tests/fixtures/file2_flat_structure.yml';
 
-        $firstPathJson = 'tests/fixtures/file1_tree_structure.json';
-        $secondPathJson = 'tests/fixtures/file2_tree_structure.json';
-
-        $firstPathYaml = 'tests/fixtures/file1_tree_structure.yaml';
-        $secondPathYaml = 'tests/fixtures/file2_tree_structure.yaml';
-
         $expectedStylishFlat = trim(file_get_contents($this->
         getFixtureFullPath('result_stylish_formatter_flat_structure')));
+
+        return [
+            [$firstPathFlatJson, $secondPathFlatJson, $expectedStylishFlat],
+            [$firstPathFlatYaml, $secondPathFlatYml, $expectedStylishFlat]
+        ];
+    }
+
+    public function argumentProviderForTreeStructureStylishFormat(): array
+    {
         $expectedStylish = trim(file_get_contents($this->
         getFixtureFullPath('result_stylish_formatter_tree_structure')));
+
+        return [
+            [self::PATHTOFIRSTJSONFILES, self::PATHTOSECONDJSONFILES, $expectedStylish],
+            [self::PATHTOFIRSTJSONFILES, self::PATHTOSECONDJSONFILES, $expectedStylish, 'stylish'],
+            [self::PATHTOFIRSTYAMLFILES, self::PATHTOSECONDYAMLFILES, $expectedStylish],
+            [self::PATHTOFIRSTYAMLFILES, self::PATHTOSECONDYAMLFILES, $expectedStylish, 'stylish']
+        ];
+    }
+
+    public function argumentProviderForTreeStructurePlainFormat(): array
+    {
         $expectedPlain = trim(file_get_contents($this->
         getFixtureFullPath('result_plain_formatter_tree_structure')));
+
+        return [
+            [self::PATHTOFIRSTJSONFILES, self::PATHTOSECONDJSONFILES, $expectedPlain , 'plain'],
+            [self::PATHTOFIRSTYAMLFILES, self::PATHTOSECONDYAMLFILES, $expectedPlain, 'plain']
+        ];
+    }
+
+    public function argumentProviderForTreeStructureJsonFormat(): array
+    {
         $expectedJson = trim(file_get_contents($this->
         getFixtureFullPath('result_json-formatter_tree_structure')));
 
         return [
-            [$firstPathFlatJson, $secondPathFlatJson, $expectedStylishFlat],
-            [$firstPathJson, $secondPathJson, $expectedStylish],
-            [$firstPathJson, $secondPathJson, $expectedStylish, 'stylish'],
-            [$firstPathJson, $secondPathJson, $expectedPlain , 'plain'],
-            [$firstPathJson, $secondPathJson, $expectedJson, 'json'],
-            [$firstPathFlatYaml, $secondPathFlatYml, $expectedStylishFlat],
-            [$firstPathYaml, $secondPathYaml, $expectedStylish],
-            [$firstPathYaml, $secondPathYaml, $expectedStylish, 'stylish'],
-            [$firstPathYaml, $secondPathYaml, $expectedPlain, 'plain'],
-            [$firstPathYaml, $secondPathYaml, $expectedJson, 'json']
+            [self::PATHTOFIRSTJSONFILES, self::PATHTOSECONDJSONFILES, $expectedJson, 'json'],
+            [self::PATHTOFIRSTYAMLFILES, self::PATHTOSECONDYAMLFILES, $expectedJson, 'json']
         ];
     }
 
